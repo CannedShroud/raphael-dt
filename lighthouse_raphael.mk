d7977d9846799979c760b7b367853aa89287f680
CannedShroud <hypersync112@gmail.com>
2021-10-07 12:07:18 +0530

    Lighthouse now
    Signed-off-by: CannedShroud <hypersync112@gmail.com>

--- lighthouse_raphael.mk
+++ lighthouse_raphael.mk
@@ -14,9 +14,10 @@
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, device/xiaomi/raphael/device.mk)
+$(call inherit-product, vendor/lighthouse/config/common.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := aosp_raphael
+PRODUCT_NAME := lighthouse_raphael
 PRODUCT_DEVICE := raphael
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := MI 9T Pro
